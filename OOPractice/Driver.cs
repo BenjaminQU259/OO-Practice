03f15b94ba34b7afdc54ed333c89330db7c0ef33
Minghao QU <MQu2@slb.com>
2022-10-27 13:49:35 +0800

    feat: create driver class can return message when drive

---
+++ OOPractice/Driver.cs
@@ -0,0 +1,17 @@
+﻿namespace OOPractice
+{
+  public class Driver
+  {
+    private readonly Vehicle vehicle;
+
+    public Driver(Vehicle vehicle)
+    {
+      this.vehicle = vehicle;
+    }
+
+    public string Drive()
+    {
+      return vehicle.SpeedUp();
+    }
+  }
+}
---
+++ OOPracticeTest/DriverTest.cs
@@ -0,0 +1,36 @@
+﻿using OOPractice;
+using Xunit;
+
+namespace OOPracticeTest
+{
+  public class DriverTest
+  {
+    [Fact]
+    public void Should_return_car_message_when_drive_given_car_with_name_speed()
+    {
+      // given
+      var car = new Car("Cool Car", 30);
+      var driver = new Driver(car);
+
+      // when
+      string message = driver.Drive();
+
+      // then
+      Assert.Equal("Cool Car: speed up 30 km/h", message);
+    }
+
+    [Fact]
+    public void Should_return_truck_message_when_drive_given_truck_with_name_speed()
+    {
+      // given
+      var truck = new Truck("Big Truck", 10);
+      var driver = new Driver(truck);
+
+      // when
+      string message = driver.Drive();
+
+      // then
+      Assert.Equal("Big Truck: speed up 10 km/h", message);
+    }
+  }
+}
