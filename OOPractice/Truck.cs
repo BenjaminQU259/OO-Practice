68870a42165ff12ed807fa78b3bc6342baab1978
Minghao QU <MQu2@slb.com>
2022-10-27 11:38:51 +0800

    feat: create truck class can show message when speed up

---
+++ OOPractice/Truck.cs
@@ -0,0 +1,19 @@
+﻿namespace OOPractice
+{
+  public class Truck
+  {
+    private readonly string name;
+    private readonly int speed;
+
+    public Truck(string name, int speed)
+    {
+      this.name = name;
+      this.speed = speed;
+    }
+
+    public string SpeedUp()
+    {
+      return $"{name}: speed up {speed} km/h";
+    }
+  }
+}
--- OOPracticeTest/TruckTest.cs
+++ OOPracticeTest/TruckTest.cs
@@ -3,19 +3,19 @@ namespace OOPracticeTest
   using OOPractice;
   using Xunit;
 
-  public class CarTest
+  public class TruckTest
   {
     [Fact]
     public void Should_return_message_when_speed_up_given_car_with_name_speed()
     {
       // given
-      var car = new Car("Cool Car", 30);
+      var truck = new Truck("Big Truck", 10);
 
       // when
-      string message = car.SpeedUp();
+      string message = truck.SpeedUp();
 
       // then
-      Assert.Equal("Cool Car: speed up 30 km/h", message);
+      Assert.Equal("Big Truck: speed up 10 km/h", message);
     }
   }
 }
